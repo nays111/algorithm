fc29ce0fc536c4cb6a77acfb6c3896ca0510ecbb
nays111 <43735448+nays111@users.noreply.github.com>
2020-11-24 15:51:34 +0900

    백준10809_알파벳 찾기

---
+++ src/codup/_10809.java
@@ -0,0 +1,33 @@
+package codup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _10809 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		int[] arr = new int[26];
+		
+		for(int i=0;i<26;i++) {
+			arr[i] = -1;
+		}
+		for(int i=0;i<str.length();i++) {
+			char ch = str.charAt(i);
+			
+			if(arr[ch-'a']==-1) {
+				arr[ch-'a']=i;
+			}
+		}
+		for(int i=0;i<26;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+		
+
+	}
+
+}
