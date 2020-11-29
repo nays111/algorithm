2be0dd7cb524bee53371f685566ae9056c389bf2
nays111 <43735448+nays111@users.noreply.github.com>
2020-11-30 00:57:05 +0900

    Create level1_예산.java

---
+++ Programmers/level1_예산.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        
+        for(int i=0;i<d.length;i++){
+            if(budget-d[i]>=0){
+                budget-=d[i];
+                answer ++;
+            }else{
+                break;
+            }
+        }
+        return answer;
+    }
+}
