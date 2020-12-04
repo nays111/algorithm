f715dad98004d098c8f1a62033fe5b490be12e76
nays111 <43735448+nays111@users.noreply.github.com>
2020-12-04 18:28:24 +0900

    Create leve1_나누어 떨어지는 숫자 배열.java

---
+++ Programmers/leve1_나누어 떨어지는 숫자 배열.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = {};
+        ArrayList <Integer> list = new ArrayList<Integer>();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%divisor==0){
+                list.add(arr[i]);
+            }
+        }
+        if(list.isEmpty()){
+            list.add(-1);
+        }
+        
+        answer = new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            answer[i]=list.get(i);
+        }
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
