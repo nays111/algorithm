16a23e8ca225f40a785824235fb7261d9e0f405e
nays111 <43735448+nays111@users.noreply.github.com>
2020-12-20 23:27:56 +0900

    Create _1676_팩토리얼 0의 개수.cpp

---
+++ BOJ/_1676_팩토리얼 0의 개수.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cstdio>
+#include <stack>
+#include <queue>
+#include <map>
+#include <functional>
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+    int N;
+    cin>>N;
+    int answer=0;
+    for(int i=5;i<=N;i*=5){
+        answer+=N/i;
+    }
+    cout<<answer;
+    return 0;
+}
