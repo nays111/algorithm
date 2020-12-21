47d5c517d336f47f1050067d9c9d7a41d8f8ae75
nays111 <43735448+nays111@users.noreply.github.com>
2020-12-21 15:13:52 +0900

    Create _11727_2xn 타일링2.cpp

---
+++ BOJ/_11727_2xn 타일링2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cstdio>
+#include <stack>
+#include <queue>
+#include <map>
+#include <functional>
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+    int N;
+    cin>>N;
+    int v[10001];
+    v[0] = 0;
+    v[1] = 1;
+    v[2] = 3;
+    for(int i=3;i<=N;i++){
+        v[i] = (v[i-1])%10007+(v[i-2]*2)%10007;
+    }
+    cout<<(v[N]%10007);
+    return 0;
+}
