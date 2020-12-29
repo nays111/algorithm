e63a74906057c68273376376162199c7a25e47fc
nays111 <43735448+nays111@users.noreply.github.com>
2020-12-29 21:45:12 +0900

    Create _1402_아무래도 이 문제는 A번 난이도인것 같다.cpp

---
+++ BOJ/_1402_아무래도 이 문제는 A번 난이도인것 같다.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cstdio>
+#include <stack>
+#include <queue>
+#include <map>
+#include <functional>
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);  
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        int a,b;
+        cin>>a>>b;
+        cout<<"yes";
+    }
+    
+
+    return 0;
+}
