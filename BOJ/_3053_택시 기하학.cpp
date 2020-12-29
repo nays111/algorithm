8ea04fd50484fb78dba7969002b8030599a8dec4
nays111 <43735448+nays111@users.noreply.github.com>
2020-12-29 21:45:30 +0900

    Create _3053_택시 기하학.cpp

---
+++ BOJ/_3053_택시 기하학.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cstdio>
+#include <stack>
+#include <queue>
+#include <map>
+#include <functional>
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);  
+    double r;
+    cin>>r;
+    cout<<fixed;
+    cout.precision(6);
+    cout<<r*r*M_PI<<'\n';
+    cout<<r*r*2;
+
+    return 0;
+}
